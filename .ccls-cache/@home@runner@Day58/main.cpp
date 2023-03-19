d57a3ddcabb2bd72c3a1becda219af3b84ff3f36
Jayanta Roy <93045961+jayanta8509@users.noreply.github.com>
2023-03-19 13:39:11 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Day58/main.cpp
@@ -0,0 +1,52 @@
+/*Bucket Filling
+
+Nejiya has a bucket having a capacity of K liters. It is already filled with X
+liters of water.
+
+Find the maximum amount of extra water in liters that Nejiya can fill in the
+bucket without overflowing.
+
+Input Format
+
+The first line will contain T - the number of test cases. Then the test cases
+follow.
+
+The first and only line of each test case contains two space separated integers
+K and X - as mentioned in the problem.
+
+Output Format
+
+For each test case, output in a single line, the amount of extra water in liters
+that Nejiya can fill in the bucket without overflowing.
+
+Sample Input 1
+
+2
+
+5 4
+
+15 6
+
+Sample Output 1
+
+1
+
+9
+*/
+
+#include <iostream>
+using namespace std;
+void Bucket_Filling(int a) {
+  while (a--) {
+    int k, b;
+    cin >> k >> b;
+    int l = k - b;
+    cout << l << endl;
+  }
+}
+
+int main() {
+  int a;
+  cin >> a;
+  Bucket_Filling(a);
+}
